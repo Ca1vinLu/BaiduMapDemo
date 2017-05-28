6282a747dad93a74049be46dfad3438a2eba00d1
LYZ <q751264263@vip.qq.com>
2017-05-28 13:36:58 +0800

    first

---
+++ app/src/main/java/com/example/lyz/baidumapdemo/MainActivity.java
@@ -0,0 +1,455 @@
+package com.example.lyz.baidumapdemo;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.trace.LBSTraceClient;
+import com.baidu.trace.Trace;
+import com.baidu.trace.model.OnTraceListener;
+import com.baidu.trace.model.PushMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int SDK_PERMISSION_REQUEST = 127;
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    private String permissionInfo;
+
+    //--定位参数
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+    private boolean isFirstLocate=true;
+    private MyLocationConfiguration.LocationMode mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;
+    private double mCurrentLatitude;
+    private double mCurrentLongitude;
+    private float mXDirection;
+    private float mCurrentAccracy;
+
+    private MyOrientationListener myOrientationListener;
+    //--轨迹参数
+
+    // 轨迹服务ID
+    private long serviceId = 141814;
+    // 设备标识
+    private String entityName = "myTrace";
+    // 是否需要对象存储服务，默认为：false，关闭对象存储服务。注：鹰眼 Android SDK v3.0以上版本支持随轨迹上传图像等对象数据，若需使用此功能，该参数需设为 true，且需导入bos-android-sdk-1.0.2.jar。
+    private boolean isNeedObjectStorage = false;
+    // 初始化轨迹服务
+    private Trace mTrace;
+    // 初始化轨迹服务客户端
+    private LBSTraceClient mTraceClient;
+    private OnTraceListener mTraceListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+
+        initView();
+        initLocation();
+        getPersimmions();
+        initTrace();
+        initOritationListener();
+
+
+    }
+
+    private void initOritationListener()
+    {
+        myOrientationListener = new MyOrientationListener(
+                getApplicationContext());
+        myOrientationListener
+                .setOnOrientationListener(new MyOrientationListener.OnOrientationListener()
+                {
+                    @Override
+                    public void onOrientationChanged(float x)
+                    {
+                        mXDirection = (int) x;
+
+                        Log.d("direction",String.valueOf(mXDirection));
+                        // 构造定位数据
+                        MyLocationData locData = new MyLocationData.Builder()
+                                .accuracy(mCurrentAccracy)
+                                // 此处设置开发者获取到的方向信息，顺时针0-360
+                                .direction(mXDirection)
+                                .latitude(mCurrentLatitude)
+                                .longitude(mCurrentLongitude).build();
+                        // 设置定位数据
+                        mBaiduMap.setMyLocationData(locData);
+                        // 设置自定义图标
+                        BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory
+                                .fromResource(R.drawable.direction);
+                        MyLocationConfiguration config = new MyLocationConfiguration(
+                                mCurrentMode, true, mCurrentMarker);
+                        mBaiduMap.setMyLocationConfiguration(config);
+
+                    }
+                });
+    }
+
+    private void initTrace() {
+        mTrace = new Trace(serviceId, entityName, isNeedObjectStorage);
+        mTraceClient = new LBSTraceClient(getApplicationContext());
+        // 初始化轨迹服务监听器
+         mTraceListener = new OnTraceListener() {
+
+            @Override
+            public void onBindServiceCallback(int i, String s) {
+
+            }
+
+            // 开启服务回调
+            @Override
+            public void onStartTraceCallback(int status, String message) {
+            }
+
+            // 停止服务回调
+            @Override
+            public void onStopTraceCallback(int status, String message) {
+            }
+
+            // 开启采集回调
+            @Override
+            public void onStartGatherCallback(int status, String message) {
+            }
+
+            // 停止采集回调
+            @Override
+            public void onStopGatherCallback(int status, String message) {
+            }
+
+            // 推送回调
+            @Override
+            public void onPushCallback(byte messageNo, PushMessage message) {
+            }
+        };
+
+
+
+    }
+
+    private void initView() {
+        mMapView = (MapView) findViewById(R.id.bmapView);
+
+        mBaiduMap = mMapView.getMap();
+
+
+        //普通地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(18.5f);
+        mBaiduMap.setMapStatus(msu);
+
+        //卫星地图
+//        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+
+        //空白地图, 基础地图瓦片将不会被渲染。在地图类型中设置为NONE，将不会使用流量下载基础地图瓦片图层。使用场景：与瓦片图层一起使用，节省流量，提升自定义瓦片图下载速度。
+//        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NONE);
+
+
+    }
+
+    private void initLocation() {
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);
+        //注册监听函数
+
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+
+        option.setCoorType("bd09ll");
+        //可选，默认gcj02，设置返回的定位结果坐标系
+
+        int span = 20000;
+        option.setScanSpan(span);
+        //可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+
+        option.setIsNeedAddress(true);
+        //可选，设置是否需要地址信息，默认不需要
+
+        option.setOpenGps(true);
+        //可选，默认false,设置是否使用gps
+
+        option.setLocationNotify(true);
+        //可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+
+        option.setIsNeedLocationDescribe(true);
+        //可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+
+        option.setIsNeedLocationPoiList(true);
+        //可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+
+        option.setIgnoreKillProcess(false);
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+
+        option.SetIgnoreCacheException(false);
+        //可选，默认false，设置是否收集CRASH信息，默认收集
+
+        option.setEnableSimulateGps(false);
+        //可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+
+        mLocationClient.setLocOption(option);
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+
+        }
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location==null||mMapView==null)
+                return;
+            MyLocationData myLocationData = new MyLocationData.Builder().accuracy(location.getRadius()).latitude(location.getLatitude()).longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(myLocationData);
+
+            mCurrentLatitude = location.getLatitude();
+            mCurrentLongitude = location.getLongitude();
+            mCurrentAccracy=location.getRadius();
+
+
+            if (isFirstLocate)
+            {
+                isFirstLocate = false;
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(latLng);
+                mBaiduMap.animateMapStatus(u);
+            }
+
+
+
+            BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory
+                    .fromResource(R.drawable.direction);
+            MyLocationConfiguration config = new MyLocationConfiguration(mCurrentMode, true, mCurrentMarker);
+            mBaiduMap.setMyLocationConfiguration(config);
+
+            //获取定位结果
+            StringBuffer sb = new StringBuffer(256);
+
+            sb.append("time : ");
+            sb.append(location.getTime());    //获取定位时间
+
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());    //获取类型类型
+
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());    //获取纬度信息
+
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());    //获取经度信息
+
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());    //获取定位精准度
+
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {
+
+                // GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());    // 单位：公里每小时
+
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());    //获取卫星数
+
+                sb.append("\nheight : ");
+                sb.append(location.getAltitude());    //获取海拔高度信息，单位米
+
+                sb.append("\ndirection : ");
+                sb.append(location.getDirection());    //获取方向信息，单位度
+
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());    //获取地址信息
+
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+
+                // 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());    //获取地址信息
+
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());    //获取运营商信息
+
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+
+                // 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+
+            }
+
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());    //位置语义化信息
+
+            List<Poi> list = location.getPoiList();    // POI数据
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+
+            Log.i("BaiduLocationApiDem", sb.toString());
+        }
+    }
+
+    private void centreToMyLoc()
+    {
+        LatLng latLng = new LatLng(mCurrentLatitude, mCurrentLongitude);
+        MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(latLng);
+        mBaiduMap.animateMapStatus(u);
+    }
+
+
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+            /*
+             * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+			 */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            // 读取电话状态权限
+            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+            }
+
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(23)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+            if (shouldShowRequestPermissionRationale(permission)) {
+                return true;
+            } else {
+                permissionsList.add(permission);
+                return false;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // TODO Auto-generated method stub
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mBaiduMap.setMyLocationEnabled(true);
+        if (!mLocationClient.isStarted())
+            mLocationClient.start();
+        myOrientationListener.start();
+        mTraceClient.startTrace(mTrace, mTraceListener);
+        mTraceClient.startGather(mTraceListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mLocationClient.stop();
+        myOrientationListener.stop();
+        mTraceClient.stopTrace(mTrace, mTraceListener);
+        mTraceClient.stopGather(mTraceListener);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/lyz/baidumapdemo/MapApplication.java
@@ -0,0 +1,12 @@
+package com.example.lyz.baidumapdemo;
+
+import android.app.Application;
+
+/**
+ * Created by LYZ on 2017/5/26 0026.
+ */
+
+public class MapApplication extends Application {
+
+
+}
